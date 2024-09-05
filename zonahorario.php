8df72a929fc7efece253491db09ddb7b47df3d15
octaviorgc <octavio199710@hotmail.com>
2024-09-05 14:42:14 -0400

    ejercicio2

---
+++ zonahorario.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+date_default_timezone_set("America/Lima");
+$fecha = date("d-m-Y");
+$hora = date("H:i:s");
+
+echo "Hora de America/Lima<br>";
+echo "La fecha actual es: $fecha <br>";
+echo "La hora actual es: $hora";
+
+?>
+
+</body>
+</html>

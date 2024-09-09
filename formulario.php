e7c24e59c2d798a248dba64fcd6753b309fed199
octaviorgc <octavio199710@hotmail.com>
2024-09-09 00:56:17 -0400

    commit

--- formulario.php
+++ formulario.php
@@ -1,34 +1,17 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-</head>
 <body>
-    <form action="" method="post">
-        <label for="fecha">Fecha:</label>
-        <input type="date" name="fecha" id="fecha">
-        <input type="submit" value="Comprobar">
-    </form>
-</body>
+
 <?php
+date_default_timezone_set("America/Lima");
+$fecha = date("d-m-Y");
+$hora = date("H:i:s");
 
-if (isset($_POST["fecha"])) 
-{
-    $fecha = $_POST["fecha"];
-
-    // Convertir la fecha a un objeto DateTime
-    $fecha_objeto = new DateTime($fecha);
-
-    // Obtener el mes de la fecha
-    $mes = $fecha_objeto->format("m");
-
-    // Comprobar si la fecha es verano
-    if ($mes >= 6 && $mes <= 9) {
-        echo "Es verano";
-    } else {
-        echo "No es verano";
-    }
-}
+echo "Hora de America/Lima<br>";
+echo "La fecha actual es: $fecha <br>";
+echo "La hora actual es: $hora";
 
 ?>
+
+</body>
 </html>
--- zonahorario.php
+++ zonahorario.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html>
 <html>
 <body>
-
 <?php
-date_default_timezone_set("America/Lima");
-$fecha = date("d-m-Y");
-$hora = date("H:i:s");
 
-echo "Hora de America/Lima<br>";
-echo "La fecha actual es: $fecha <br>";
-echo "La hora actual es: $hora";
+// Función para obtener la fecha actual
+function obtenerFechaActual() {
+    return date("d-m-Y");
+}
+
+// Función para obtener la hora actual
+function obtenerHoraActual() {
+    return date("H:i:s");
+}
+
+// Función para mostrar la fecha y hora
+function mostrarFechaYHora() {
+    $fecha = obtenerFechaActual();
+    $hora = obtenerHoraActual();
+
+    echo "Hora de servidor<br>";
+    echo "La fecha actual es: $fecha <br>";
+    echo "La hora actual es: $hora";
+}
+
+// Llamada a la función para mostrar la fecha y hora
+mostrarFechaYHora();
 
 ?>
-
 </body>
 </html>
